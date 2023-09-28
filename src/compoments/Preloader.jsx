d3e256c321f55d9446e813ef9ae81319754ac907
Sandeep-Luhaniwal <sandeepluh0001.com>
2023-09-28 12:10:12 +0530

    change

--- src/compoments/Preloader.jsx
+++ src/compoments/Preloader.jsx
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from "react";
-import Logo from '../assets/images/png/Main_logo.png'
+import React, { useEffect, useState } from 'react'
+import reloder_img from '../assets/images/png/Main_logo.png'
 
-const Preloader = () => {
+function Preloder() {
     const [loder, setloader] = useState(true);
     useEffect(() => {
         setTimeout(() => {
             setloader(false);
-        }, 3500);
+        }, 2500);
     }, []);
     {
         if (loder === true) {
@@ -18,16 +18,11 @@ const Preloader = () => {
     return (
         <div>
             {loder ? (
-                <section
-                    style={{ zIndex: "999999999" }}
-                    className="bg-black top-0 start-0 position-fixed h-100 w-100 d-flex flex-column justify-content-center align-items-center"
-                >
+                <section className="bg-black top-0 start-0 position-fixed h-100 w-100 d-flex flex-column justify-content-center align-items-center">
                     <div className="d-flex justify-content-center align-items-center flex-column text-center">
                         <div className="loader">
                             <div className="d-flex justify-content-center align-items-center flex-column">
-                                <span>
-                                    <img className="wordpressLoader w-100" src={Logo} alt="Preloader" />
-                                </span>
+                                <img className="wordpressLoader w-100" src={reloder_img} alt="reloder_img" />
                                 <div className="d-flex justify-content-center align-items-center mt-5">
                                     <h2 className="ff_primary color_white fs_lg fw_semiBold ">UNLOCK YOUR <span className="fc-primary fw-900">POTENTIAL CV</span> </h2>
                                 </div>
