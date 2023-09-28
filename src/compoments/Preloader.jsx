c96e1fff1c66d6698cd84bc2a84431d530916c01
Sandeep-Luhaniwal <sandeepluh0001.com>
2023-09-28 12:14:32 +0530

    change

--- src/compoments/Preloader.jsx
+++ src/compoments/Preloader.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import reloder_img from '../assets/images/png/Main_logo.png'
 
-function Preloder() {
+function Preloader() {
     const [loder, setloader] = useState(true);
     useEffect(() => {
         setTimeout(() => {
