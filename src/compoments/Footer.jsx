b93ffcc7b3328ea6fe41b89006e3dc74dc961a1a
Sandeep-Luhaniwal <sandeepluh0001.com>
2023-09-28 12:02:05 +0530

    Link aadd

--- src/compoments/Footer.jsx
+++ src/compoments/Footer.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import mainLogo from '../assets/images/png/FooterLogo.png'
 import { Col, Row } from 'react-bootstrap'
 import { CallIcon, FacbookIcon, InstagramIcon, MessageIcon, TwitterIcon } from './Icon'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
     return (
@@ -24,26 +25,26 @@ const Footer = () => {
                             <div className='pt-md-3 d-flex justify-content-between pt-4 mt-1 mt-sm-0'>
                                 <ul className='mb-0 ps-0 d-flex flex-column gap-20 '>
                                     <li className='ff_primary fs_sm fw_medium color_main'>Quick Links</li>
-                                    <li><a href="#" className='ff_primary fs_sm fw_regular op-9 color_main hoverfooter'>Home</a></li>
-                                    <li><a href="#" className='ff_primary fs_sm fw_regular op-9 color_main hoverfooter'>About</a></li>
-                                    <li><a href="#" className='ff_primary fs_sm fw_regular op-9 color_main hoverfooter'>We’ll help you</a></li>
-                                    <li><a href="#" className='ff_primary fs_sm fw_regular op-9 color_main hoverfooter'>Testimonials</a></li>
+                                    <li><Link className='ff_primary fs_sm fw_regular op-9 color_main hoverfooter'>Home</Link></li>
+                                    <li><Link className='ff_primary fs_sm fw_regular op-9 color_main hoverfooter'>About</Link></li>
+                                    <li><Link className='ff_primary fs_sm fw_regular op-9 color_main hoverfooter'>We’ll help you</Link></li>
+                                    <li><Link className='ff_primary fs_sm fw_regular op-9 color_main hoverfooter'>Testimonials</Link></li>
                                 </ul>
                                 <ul className='mb-0 ps-0 d-flex flex-column gap-20 '>
                                     <li className='ff_primary fs_sm fw_medium color_main'>Support</li>
-                                    <li><a href="#" className='ff_primary fs_sm fw_regular op-9 color_main hoverfooter'><CallIcon /><span className='ps-1'>+012 345 67890</span></a></li>
-                                    <li><a href="#" className='ff_primary fs_sm fw_regular op-9 color_main hoverfooter'><MessageIcon /><span className='ps-1'>Cvhawk@gmail.com</span></a></li>
+                                    <li><Link className='ff_primary fs_sm fw_regular op-9 color_main hoverfooter'><CallIcon /><span className='ps-1'>+012 345 67890</span></Link></li>
+                                    <li><Link className='ff_primary fs_sm fw_regular op-9 color_main hoverfooter'><MessageIcon /><span className='ps-1'>Cvhawk@gmail.com</span></Link></li>
                                 </ul>
                                 <ul className='mb-0 ps-0 d-flex flex-column gap-20 d-none d-sm-block '>
                                     <li className='ff_primary fs_sm fw_medium color_main'>Legal</li>
-                                    <li><a href="#" className='ff_primary fs_sm fw_regular op-9 color_main hoverfooter'>Privacy Policy</a></li>
-                                    <li><a href="#" className='ff_primary fs_sm fw_regular op-9 color_main hoverfooter'>Terms & conditions</a></li>
+                                    <li><Link className='ff_primary fs_sm fw_regular op-9 color_main hoverfooter'>Privacy Policy</Link></li>
+                                    <li><Link className='ff_primary fs_sm fw_regular op-9 color_main hoverfooter'>Terms & conditions</Link></li>
                                 </ul>
                             </div>
                             <ul className='mb-0 ps-0 d-flex flex-column gap-20 pt-4 mt-1 d-sm-none'>
                                 <li className='ff_primary fs_sm fw_medium color_main'>Legal</li>
-                                <li><a href="#" className='ff_primary fs_sm fw_regular op-9 color_main hoverfooter'>Privacy Policy</a></li>
-                                <li><a href="#" className='ff_primary fs_sm fw_regular op-9 color_main hoverfooter'>Terms & conditions</a></li>
+                                <li><Link className='ff_primary fs_sm fw_regular op-9 color_main hoverfooter'>Privacy Policy</Link></li>
+                                <li><Link className='ff_primary fs_sm fw_regular op-9 color_main hoverfooter'>Terms & conditions</Link></li>
                             </ul>
                             <div className='d-flex gap-32 pt-4 mt-1 d-sm-none'>
                                 <a className='footerIcon' href="https://twitter.com/"> <TwitterIcon /></a>
@@ -54,11 +55,11 @@ const Footer = () => {
                     </Row>
                 </div>
             </section>
-            <section className='bg_footer bordertop'>
+            <div className='bg_footer bordertop'>
                 <div className='py-3 md:my-1'>
                     <p className='ff_primary fs_xs fw_regular color_main my-0 text-center'>Copyright@CVhawk2023</p>
                 </div>
-            </section>
+            </div>
         </>
     )
 }
