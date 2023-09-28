50738ea7af4ae04800968082420b10495e5bff71
Sandeep-Luhaniwal <sandeepluh0001.com>
2023-09-28 12:16:15 +0530

    z index add

--- src/compoments/Preloader.jsx
+++ src/compoments/Preloader.jsx
@@ -18,7 +18,7 @@ function Preloader() {
     return (
         <div>
             {loder ? (
-                <section className="bg-black top-0 start-0 position-fixed h-100 w-100 d-flex flex-column justify-content-center align-items-center">
+                <section className="bg-black top-0 start-0 position-fixed h-100 w-100 d-flex flex-column justify-content-center align-items-center z-1000">
                     <div className="d-flex justify-content-center align-items-center flex-column text-center">
                         <div className="loader">
                             <div className="d-flex justify-content-center align-items-center flex-column">
